741eaefc5eefac211a7e34d90ad137465225517b
Guille <guillermolopez3@hotmail.com>
2019-06-26 22:28:10 -0300

    trabajando en el perfil

--- e_commerce/lib/perfil.dart
+++ e_commerce/lib/perfil.dart
@@ -20,15 +20,54 @@ class Perfil extends StatelessWidget {
         centerTitle: true,
         elevation: 0.0,
         backgroundColor: Colors.white,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: (){
+              _showDialog(context);
+            },
+          )
+        ],
       ),
       body: _body(),
     );
   }
 
+  void _showDialog(BuildContext context){
+    showDialog(
+      context: context,
+      builder: (context){
+          return AlertDialog(
+            title: Text('Denunciar Perfil'),
+            content: TextField(
+              decoration: InputDecoration(
+                hintText: 'Motivo de la denuncia'
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Enviar', style: TextStyle(color: Colors.black)),
+                onPressed: ()=>Navigator.of(context).pop(),
+              ),
+              FlatButton(
+                child: Text('Cancelar', style: TextStyle(color: Colors.black)),
+                onPressed: ()=> Navigator.of(context).pop(),
+
+              )
+            ],
+          );
+      }
+    );
+
+  }
+
   Widget _body(){
     return ListView(
       children: <Widget>[
-        _cabecera()
+        _cabecera(),
+        _infoPerfil(),
+        _webUbicacion(),
+        _detalleServicio()
       ],
     );
   }
@@ -56,6 +95,46 @@ class Perfil extends StatelessWidget {
           ),
           Expanded(
             child: _contacto(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  //datos debajo de la cabecera
+  Widget _infoPerfil(){
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            name,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          SizedBox(height: 8,),
+          Text(
+            'Plomero, Gasista, Electricista matriculado',
+            style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400
+            ),
+          ),
+          SizedBox(height: 8.0),
+          RichText(
+            maxLines: 3,
+            text: TextSpan(
+                style: TextStyle(color: Colors.black),
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'Zonas trabajo: ',
+                      style: new TextStyle(fontWeight: FontWeight.bold)),
+                  TextSpan(text: 'Argüello, Rio Ceballos'),
+                ]
+            ),
           )
         ],
       ),
@@ -80,6 +159,7 @@ class Perfil extends StatelessWidget {
         FlatButton(
           shape: RoundedRectangleBorder(side: BorderSide(color: Colors.grey), borderRadius: BorderRadius.all(Radius.circular(5))),
           child: Text('CONTACTAR'),
+          onPressed: (){},
         )
       ],
     );
@@ -103,4 +183,61 @@ class Perfil extends StatelessWidget {
       ),
     );
   }
+
+  //direccion y web
+  Widget _webUbicacion()=> Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: <Widget>[
+      FlatButton(
+        onPressed: (){},
+        child: Row(
+          children: <Widget>[
+            Icon(Icons.location_on),
+            Text('Dirección')
+          ],
+        ),
+        shape: StadiumBorder(side: BorderSide(color: Colors.grey)),
+      ),
+      FlatButton(
+        onPressed: (){},
+        child: Row(
+          children: <Widget>[
+            Icon(Icons.language),
+            Text('Página')
+          ],
+        ),
+        shape: StadiumBorder(side: BorderSide(color: Colors.grey)),
+      )
+    ],
+  );
+
+  Widget _detalleServicio(){
+    return Padding(
+      padding: const EdgeInsets.only(left:10.0, right: 10.0, top: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Descripción de los servicios', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey)),
+          SizedBox(height: 10,),
+          Card(
+            elevation: 6,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+                style: TextStyle(
+                  letterSpacing: 2,
+
+                ),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
+
+
